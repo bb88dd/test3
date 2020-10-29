2de8f0e7ad1f2dd1f559506e79280b8bc26c1ff8
BenDaly <ben.daly@immutable.com>
2020-10-29 17:35:04 +1100

    added some comments to make things clearer.

--- main_test.py
+++ main_test.py
@@ -1,6 +1,9 @@
-from gitter import func_one
-import packs.getdata
+# Import
+from gitter import func_one  # this imports the function func_one from the module gitter
+import packs.getdata  # this imports the package getdata which contains several modules.
+
 
 func_one()
 
-packs.getdata.func_four()
+packs.getdata.func_four()  # this is one module from the getdata package.
+packs.getdata.func_four()  # this is another module from the getdata package.
